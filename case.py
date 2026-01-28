fc98634c8c91b2e46d46a383350213b708cfd53a
Shantoun <40128978+Shantoun@users.noreply.github.com>
2026-01-28 17:30:44 -0500

    Update case.py

--- case.py
+++ case.py
@@ -1,5 +1,6 @@
-import streamlit as st
 from functions.read_data import read_gsheet
+import streamlit as st
+import pandas as pd
 
 ########################## Read Data
 sheet = st.secrets["sheets"]["url"]
