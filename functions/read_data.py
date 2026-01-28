9c34317cf819b12149d60805cca62b3b024abb74
Shantoun <40128978+Shantoun@users.noreply.github.com>
2026-01-28 17:23:43 -0500

    Create read_data.py

---
+++ functions/read_data.py
@@ -0,0 +1,25 @@
+############################################################ Function to Read Google Sheets
+import json
+import gspread
+import pandas as pd
+from google.oauth2.service_account import Credentials
+
+
+def google_sheet_to_df(sheet_url, tab_name):
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets.readonly",
+        "https://www.googleapis.com/auth/drive.readonly",
+    ]
+
+    info = json.loads(st.secrets["gcp_service_account"]["raw_json"])
+    creds = Credentials.from_service_account_info(info, scopes=scopes)
+    gc = gspread.authorize(creds)
+
+    sh = gc.open_by_url(sheet_url)
+    ws = sh.worksheet(tab_name)
+
+    rows = ws.get_all_values()
+    if not rows:
+        return pd.DataFrame()
+
+    return pd.DataFrame(rows[1:], columns=rows[0])
