afc7897dc20ed5d9d2a3770554e03c66fb71fcc1
Shantoun <40128978+Shantoun@users.noreply.github.com>
2026-01-28 17:11:12 -0500

    Update case.py

--- case.py
+++ case.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import gspread
 import pandas as pd
+import json
 from google.oauth2.service_account import Credentials
 
 scopes = [
@@ -8,19 +9,15 @@ scopes = [
     "https://www.googleapis.com/auth/drive.readonly",
 ]
 
-creds = Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=scopes,
-)
+info = json.loads(st.secrets["gcp_service_account"]["raw_json"])
 
+creds = Credentials.from_service_account_info(info, scopes=scopes)
 gc = gspread.authorize(creds)
 
-# open by URL (easiest)
 sh = gc.open_by_url(SHEET_URL)
+ws = sh.worksheet(SHEET_TAB_NAME)
 
-ws = sh.worksheet(SHEET_TAB_NAME)  # or sh.sheet1
 rows = ws.get_all_values()
-
 df = pd.DataFrame(rows[1:], columns=rows[0])
 
 st.write(df)
